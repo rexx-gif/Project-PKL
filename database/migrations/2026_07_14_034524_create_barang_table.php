73e489d770302b46336788b9e5b1f0d78f7b8ee1
WisWho <Yusufmuhibuddin3@gmail.com>
2026-07-14 11:10:58 +0700

    menambahkan file migration untuk kebutuhan barang dan gudang

---
+++ database/migrations/2026_07_14_034524_create_barang_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            // cascadeOnDelete() biar kalau kategori dihapus, barangnya ikut kehapus otomatis
+            $table->foreignId('jenis_barang_id')->constrained('jenis_barang')->cascadeOnDelete();
+            $table->string('nama_barang');
+            $table->integer('harga_beli')->default(0);
+            $table->integer('harga_jual')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang');
+    }
+};
