b3628a044300fdece2087fb32ea2e8590e941583
Fais Adhyasta Pratama <faizadhyastapratama@gmail.com>
2026-07-14 13:33:53 +0700

    menambahkan file migration dan model customer

---
+++ database/migrations/2026_07_14_062224_create_customers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_customer')->unique();
+            $table->string('nama_customer');
+            $table->text('alamat')->nullable();
+            $table->string('no_telp')->nullable();
+            $table->string('email')->nullable()->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
